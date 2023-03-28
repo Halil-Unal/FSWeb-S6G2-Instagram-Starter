9a57bd056ffc50c5323faaed6a44b33fa677b303
HALİL ÜNAL <125642299+Halil-Unal@users.noreply.github.com>
2023-03-28 19:24:31 +0300

    sss

--- src/App.js
+++ src/App.js
@@ -5,35 +5,38 @@
 */
 
 // State hook u import edin
-import React from 'react';
+import React, { useState } from "react";
 
 // Gönderiler (çoğul!) ve AramaÇubuğu bileşenlerini import edin, çünkü bunlar App bileşeni içinde kullanılacak
 // sahteVeri'yi import edin
-import './App.css';
+import sahteVeri from "./sahte-veri";
+import Gönderiler from "./bileşenler/Gönderiler/Gönderiler";
+import AramaÇubuğu from "./bileşenler/AramaÇubuğu/AramaÇubuğu";
+
+import "./App.css";
 
 const App = () => {
+  const [gonderiler, setGonderi] = useState(sahteVeri);
   // Gönderi nesneleri dizisini tutmak için "gonderiler" adlı bir state oluşturun, **sahteVeri'yi yükleyin**.
   // Artık sahteVeri'ye ihtiyacınız olmayacak.
   // Arama çubuğunun çalışması için , arama kriterini tutacak başka bir state'e ihtiyacımız olacak.
-	
-  const gonderiyiBegen = gonderiID => {
-    /*
-      Bu fonksiyon, belirli bir id ile gönderinin beğeni sayısını bir artırma amacına hizmet eder.
 
-      Uygulamanın durumu, React ağacının en üstünde bulunur, ancak iç içe geçmiş bileşenlerin stateleri değiştirememesi adil olmaz!
-      Bu fonksiyon, belirli bir gönderinin beğeni sayısını artırılmasına olanak sağlamak amacıyla iç içe geçmiş bileşenlere aktarılır.
-
-	  "setGonderi" yi çağırın ve state ine "posts.map" çağrısını iletin.
-      `map` içine iletilen callback aşağıdaki mantığı gerçekleştirir:
-        - gönderinin idsi "gonderiID" ile eşleşirse, istenen değerlerle yeni bir gönderi nesnesi döndürün.
-        - aksi takdirde, sadece gönderi nesnesini değiştirmeden döndürün.
-     */
+  const gonderiyiBegen = (gonderiID) => {
+    const updGond = [...gonderiler];
+    updGond.map((g) => {
+      if (g.id == gonderiID) {
+        g.likes = g.likes + 1;
+      }
+    });
+    setGonderi(updGond);
   };
 
   return (
-    <div className='App'>
+    <div className="App">
       {/* AramaÇubuğu ve Gönderiler'i render etmesi için buraya ekleyin */}
+      <AramaÇubuğu />
       {/* Her bileşenin hangi proplara ihtiyaç duyduğunu kontrol edin, eğer ihtiyaç varsa ekleyin! */}
+      <Gönderiler postLikeFnCb={gonderiyiBegen} posts={gonderiler} />
     </div>
   );
 };
